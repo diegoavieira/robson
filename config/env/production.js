85d8e2b9c80b5adb4a08c04de2e9eb958d215a29
Diego A. Vieira <diegoavieira@gmail.com>
2017-10-04 22:37:53 -0300

    created connect db server

---
+++ config/env/production.js
@@ -0,0 +1,14 @@
+module.exports = {
+	db: {
+		database: 'robson',
+		username: 'postgres',
+		password: 'rob%dav',
+		params: {
+			dialect: 'postgres',
+			define: {
+				underscored: true
+			}
+		}
+	},
+	secret: 'lkjhklasd$%ÿu1439weqeFGGrilj',
+};
